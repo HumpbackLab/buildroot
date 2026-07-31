f7451107920c0e9992632876921470d898a83bda
ncerzzk <huangcmzzk@gmail.com>
2026-08-01 00:17:13 +0800

    package: add penguin-flight-console

---
+++ package/rockchip/penguin-flight-console/penguin-flight-console.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# penguin-flight-console
+#
+################################################################################
+
+PENGUIN_FLIGHT_CONSOLE_VERSION = local
+PENGUIN_FLIGHT_CONSOLE_SITE = $(TOPDIR)/../tools/penguin-flight-console
+PENGUIN_FLIGHT_CONSOLE_SITE_METHOD = local
+PENGUIN_FLIGHT_CONSOLE_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS = \
+	--exclude=/target \
+	--exclude=/dev-config \
+	--exclude=/dev-run
+PENGUIN_FLIGHT_CONSOLE_LICENSE = MIT OR Apache-2.0
+PENGUIN_FLIGHT_CONSOLE_DEPENDENCIES = wpa_supplicant
+
+define PENGUIN_FLIGHT_CONSOLE_BUILD_CMDS
+	test -x $(@D)/dist/penguin-flight-console || \
+		(echo "Run tools/penguin-flight-console/build-cross.sh first" >&2; false)
+endef
+
+define PENGUIN_FLIGHT_CONSOLE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/dist/penguin-flight-console \
+		$(TARGET_DIR)/usr/bin/penguin-flight-console
+	$(INSTALL) -d \
+		$(TARGET_DIR)/usr/libexec/penguin-flight-console/powerfin
+	$(INSTALL) -m 0755 $(@D)/boards/powerfin/*.sh \
+		$(TARGET_DIR)/usr/libexec/penguin-flight-console/powerfin/
+endef
+
+define PENGUIN_FLIGHT_CONSOLE_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 \
+		$(PENGUIN_FLIGHT_CONSOLE_PKGDIR)/S53penguin-flight-console \
+		$(TARGET_DIR)/etc/init.d/S53penguin-flight-console
+endef
+
+$(eval $(generic-package))
