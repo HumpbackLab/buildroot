d6090647ecf952bdcf4ee0c4d995086bd34f0511
ncerzzk <huangcmzzk@gmail.com>
2026-08-08 09:38:19 +0800

    penguin-flight-console: install v0.1.0 release

--- package/rockchip/penguin-flight-console/penguin-flight-console.mk
+++ package/rockchip/penguin-flight-console/penguin-flight-console.mk
@@ -4,27 +4,27 @@
 #
 ################################################################################
 
-PENGUIN_FLIGHT_CONSOLE_VERSION = refs/remotes/origin/master
-PENGUIN_FLIGHT_CONSOLE_SITE = ssh://git@github.com/HumpbackLab/penguin-flight-console.git
-PENGUIN_FLIGHT_CONSOLE_SITE_METHOD = git
+PENGUIN_FLIGHT_CONSOLE_VERSION = 0.1.0
+PENGUIN_FLIGHT_CONSOLE_SOURCE = penguin-flight-console-update.tar.gz
+PENGUIN_FLIGHT_CONSOLE_SITE = https://gitee.com/ncer/penguin-flight-console/releases/download/v$(PENGUIN_FLIGHT_CONSOLE_VERSION)
+PENGUIN_FLIGHT_CONSOLE_STRIP_COMPONENTS = 0
 PENGUIN_FLIGHT_CONSOLE_LICENSE = Proprietary
-PENGUIN_FLIGHT_CONSOLE_LICENSE_FILES = LICENSE
 PENGUIN_FLIGHT_CONSOLE_DEPENDENCIES = wpa_supplicant
 
 define PENGUIN_FLIGHT_CONSOLE_BUILD_CMDS
-	cd $(@D) && ./build-cross.sh
-	test -x $(@D)/boards/powerfin/pfc-update.sh || \
+	cd $(@D) && sha256sum -c SHA256SUMS
+	test -x $(@D)/scripts/pfc-update.sh || \
 		(echo "Missing PowerFin PFC update installer" >&2; false)
 endef
 
 define PENGUIN_FLIGHT_CONSOLE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/dist/penguin-flight-console \
+	$(INSTALL) -D -m 0755 $(@D)/penguin-flight-console \
 		$(TARGET_DIR)/usr/bin/penguin-flight-console
 	$(INSTALL) -d \
 		$(TARGET_DIR)/usr/libexec/penguin-flight-console/powerfin
-	$(INSTALL) -m 0755 $(@D)/boards/powerfin/*.sh \
+	$(INSTALL) -m 0755 $(@D)/scripts/*.sh \
 		$(TARGET_DIR)/usr/libexec/penguin-flight-console/powerfin/
-	$(INSTALL) -m 0644 $(@D)/boards/powerfin/board.conf \
+	$(INSTALL) -m 0644 $(@D)/board.conf \
 		$(TARGET_DIR)/usr/libexec/penguin-flight-console/powerfin/board.conf
 endef
 
